e2e44ab3aa61c9729f08ff966a3b910409ce05ab
Sean Walsh <seanmichwalsh@protonmail.ch>
2018-03-07 18:50:44 -0500

    Initialized login screen container

---
+++ frontend/containers/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Login = () => (
+    <Router>
+        <div>
+            <h>Login Page</h>
+        </div>
+    </Router>
+);
+
+export default Login;
