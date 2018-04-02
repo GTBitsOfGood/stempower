503f36a55789e06d294f884e0b524ac9cbfe35ef
Sean Walsh <seanmichwalsh@protonmail.ch>
2018-04-02 19:45:10 -0400

    Refreshed backend routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,8 +7,8 @@ const {check, oneOf, validationResult} = require('express-validator/check');
 
 //local imports
 const user = require('./user');
-
-const db = 'C:\data\db\stempower';
+const mentor = require('./mentor');
+const db = require('/data/db');
 
 mongoose.Promilse = global.Promise;
 mongoose.connect(db);
@@ -21,11 +21,10 @@ router.use(bodyParser.urlencoded({
 
 router.route('/').get((req, res) => {
     console.log("Basic '/' in index.js works!");
-    res.send("Basic '/' in index.js works!");
 })
 
 //RESTful endpoints (currently just user)
 router.use('/user', user);
-router.use('./mentor', Mentor);
+router.use('/mentor', mentor);
 
 module.exports = router;
