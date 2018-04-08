45cf0ba02e94fd38345705fb54bdfb23e6fcefa2
Sean Walsh <seanmichwalsh@protonmail.ch>
2018-04-08 18:03:40 -0400

    WelcomeScreen html now renders, still needs css

--- frontend/containers/WelcomePage.js
+++ frontend/containers/WelcomePage.js
@@ -8,69 +8,70 @@ import Signup from '../components/Signup';
 
 const WelcomeScreen = (props) => {
     return (
-        <div class="container">
-    		<div class="row">
-				<div class="col-md-6 col-md-offset-3">
-					<div class="panel panel-login">
-						<div class="panel-heading">
-							<div class="row">
-								<div class="col-xs-6">
-									<a href="#" class="active" id="login-form-link">Login</a>
+        <div className="container">
+    		<div className="row">
+				<div className="col-md-6 col-md-offset-3">
+					<div className="panel panel-login">
+						<div className="panel-heading">
+							<div className="row">
+								<div className="col-xs-6">
+									<a href="#" className="active" id="login-form-link">Login</a>
 								</div>
-								<div class="col-xs-6">
+								<div className="col-xs-6">
 									<a href="#" id="register-form-link">Register</a>
 								</div>
 							</div>
 						<hr/>
 						</div>
-						<div class="panel-body">
-							<div class="row">
-								<div class="col-lg-12">
-									<form id="login-form" action="https://phpoll.com/login/process" method="post" role="form" style="display: block;">
-									<div class="form-group">
-										<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value=""/>
+						<div className="panel-body">
+							<div className="row">
+								<div className="col-lg-12">
+								<form id="login-form" action="localhost:3000/profile" method="get" role="form">
+									<div className="form-group">
+										<input type="text" name="username" id="username" tabindex="1" className="form-control" placeholder="Username" value=""/>
 									</div>
-									<div class="form-group">
-										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password"/>
+									<div className="form-group">
+										<input type="password" name="password" id="password" tabindex="2" className="form-control" placeholder="Password"/>
 									</div>
-									<div class="form-group text-center">
-										<input type="checkbox" tabindex="3" class="" name="remember" id="remember"/>
+									<div className="form-group text-center">
+										<input type="checkbox" tabindex="3" className="" name="remember" id="remember"/>
 										<label for="remember">Remember Me</label>
 									</div>
-									<div class="form-group">
-										<div class="row">
-											<div class="col-sm-6 col-sm-offset-3">
-												<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In"/>
+									<div className="form-group">
+										<div className="row">
+											<div className="col-sm-6 col-sm-offset-3">
+												<input type="submit" name="login-submit" id="login-submit" tabindex="4" className="form-control btn btn-login" value="Log In"/>
 											</div>
 										</div>
 									</div>
-										<div class="form-group">
-											<div class="row">
-												<div class="col-lg-12">
-													<div class="text-center">
-														<a href="https://phpoll.com/recover" tabindex="5" class="forgot-password">Forgot Password?</a>
+										<div className="form-group">
+											<div className="row">
+												<div className="col-lg-12">
+													<div className="text-center">
+														<a href="localhost:3000/profile" tabindex="5" className="forgot-password">Forgot Password?</a>
 													</div>
 												</div>
 											</div>
 										</div>
 									</form>
-									<form id="register-form" action="https://phpoll.com/register/process" method="post" role="form" style="display: none;">
-										<div class="form-group">
-											<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value=""/>
+
+									<form id="register-form" action="localhost:3000/profile" method="post" role="form">
+										<div className="form-group">
+											<input type="text" name="username" id="username" tabindex="1" className="form-control" placeholder="Username" value=""/>
 										</div>
-										<div class="form-group">
-											<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value=""/>
+										<div className="form-group">
+											<input type="email" name="email" id="email" tabindex="1" className="form-control" placeholder="Email Address" value=""/>
 										</div>
-										<div class="form-group">
-											<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password"/>
+										<div className="form-group">
+											<input type="password" name="password" id="password" tabindex="2" className="form-control" placeholder="Password"/>
 										</div>
-										<div class="form-group">
-											<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password"/>
+										<div className="form-group">
+											<input type="password" name="confirm-password" id="confirm-password" tabindex="2" className="form-control" placeholder="Confirm Password"/>
 										</div>
-										<div class="form-group">
-											<div class="row">
-												<div class="col-sm-6 col-sm-offset-3">
-													<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now"/>
+										<div className="form-group">
+											<div className="row">
+												<div className="col-sm-6 col-sm-offset-3">
+													<input type="submit" name="register-submit" id="register-submit" tabindex="4" className="form-control btn btn-register" value="Register Now"/>
 												</div>
 											</div>
 										</div>
