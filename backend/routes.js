4706ba3cfd210c7a023ea8c3726ca3d64a2601d3
rscsam <sam.costley@yahoo.com>
2018-09-18 18:58:33 -0400

    finished test exercise

--- backend/routes.js
+++ backend/routes.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const bp = require('body-parser');
 const router = express.Router();
+router.use(bp.json());
+
+var MongoClient = require('mongodb').MongoClient;
 
 // YOUR API ROUTES HERE
 
 // SAMPLE ROUTE
 router.use('/users', (req, res) => {
     res.json({ success: true });
+});
+
+router.post('/test', (req, res) => {
+    var testDocument = {text: req.body.input};
+    MongoClient.connect("mongodb://127.0.0.1:27017", function (err, conn) {
+            if(err) throw err;
+            var db = conn.db('stem_test');
+            db.collection('test_coll', function (err, collection) {
+            collection.insertOne(testDocument);
+        });
+    });
+    res.json({result: req.body.input});
 });
 
 module.exports = router;
--- frontend/components/Title.js
+++ frontend/components/Title.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Title = ( { name } ) => {
-    return (
-        <h1>{name}</h1>
-    );
-};
+var axios = require('axios')
+
+class Title extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {t: "nothing read yet"};
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.props.name}</h1>
+                <input type="text" onChange={this.onSampleChange.bind(this)} />
+                <button onClick={this.doBackendCall.bind(this)}>Just do it</button>
+                <p>{this.state.t}</p>
+            </div>
+        );
+    }
+
+    doBackendCall() {
+        var self = this;
+        axios.post('/api/test', {input: this.state.sample})
+        .then(function(response) {
+            self.setState({t: response.data.result});
+        });
+    }
+
+    onSampleChange(e) {
+        this.setState({ sample: e.target.value });
+    }
+}
 
 Title.propTypes = {
     name: PropTypes.string,
