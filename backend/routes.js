0cc7ac2df628bdb7ca2afa4160d0aef27ac5c12e
William Zheng <williamzhengw@yahoo.com>
2018-09-18 12:24:11 -0400

    test

--- backend/routes.js
+++ backend/routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 // YOUR API ROUTES HERE
+router.get('/test', (req, res) => {
+	res.render('test', { output: <h1> "hello </h1>" })
+});
 
 // SAMPLE ROUTE
 router.use('/users', (req, res) => {
