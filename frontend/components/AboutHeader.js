5e143e198c9563624c05794b69a45499968cc884
Ashley Tan <tan.ashley32@gmail.com>
2018-11-06 19:49:37 -0500

    integrated mentor api call for profile generation
    ***used hardcoded mentor id in Profile componentWillMount()

--- frontend/components/AboutHeader.js
+++ frontend/components/AboutHeader.js
@@ -4,41 +4,19 @@ import { fetchMentor } from '../actions/mentorActions.js'
 
 
 class AboutHeader extends React.Component {
-    constructor(props) {
-        super(props);
-        this.fields = {
-            name: props.name,
-            education: props.education,
-            email: props.email,
-            phone: props.phone
-        }
-    }
-
-    componentDidMount() {
-        this.props.dispatch(fetchMentor());
-    }
 
     render() {
         return (
             <div className="profile-about">
-                <label className="flex-item profile-name">{this.fields.name}</label>
+                <label className="flex-item profile-name">{this.props.mentor.firstName} {this.props.mentor.lastName}</label>
                 <div className="display-flex">
-                    <div className="flex-item">{this.fields.education}</div>
+                    <div className="flex-item">{this.props.mentor.university}</div>
                 </div>
-                <div className="flex-item">{this.fields.email}</div>
-                <div className="flex-item">{this.fields.phone}</div>
+                <div className="flex-item">{this.props.mentor.email}</div>
+                <div className="flex-item">{this.props.mentor.phoneNumber}</div>
             </div>
         )
     }
 };
 
-const mapStateToProps = state => {
-    return {
-        name: state.firstName + ' ' + state.lastName,
-        education: state.university,
-        email: state.email,
-        phone: state.phoneNumber
-    };
-}
-
-export default connect(mapStateToProps)(AboutHeader);
+export default AboutHeader;
