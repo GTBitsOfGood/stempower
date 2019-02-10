de86e5611c6bd48d4bf71b8c9e8440745ab70709
HemangRajvanshy <hemangrajvanshy@gmail.com>
2019-02-10 14:48:38 -0500

    Dummy profile Page WIP

--- frontend/containers/Profile.js
+++ frontend/containers/Profile.js
@@ -1,34 +1,50 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import ImageDisplay from '../components/ImageDisplay.js';
-import AboutHeader from '../components/AboutHeader.js'
-import BioItem from '../components/BioItem.js'
-import BioContainer from './BioContainer'
-
-
+import ImageDisplay from "../components/ImageDisplay.js";
+import ProfilePanel from "./ProfilePanel.js";
+import BioItem from "../components/BioItem.js";
+import BioContainer from "./BioContainer";
 
 class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mentor: {
+        bios: [],
+        firstName: "",
+        lastName: "",
+        university: "",
+        email: "",
+        phone: ""
+      }
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            mentor: {
-                bios: [],
-                firstName: "",
-                lastName: "",
-                university: ""
-            }
-        }
-    }
-    
-    //api call for mentor
-    componentWillMount() {
-      const id = this.props.match.params.id;
-        axios.get('/api/mentors/' + id).then(({ data }) => {
-          this.setState({mentor: data});
+  //api call for mentor
+  componentWillMount() {
+    const id = this.props.match.params.id;
+    axios
+      .get("/api/mentors/" + id)
+      .then(({ data }) => {
+        this.setState({ mentor: data });
       })
-    }
+      .catch(error => {
+        console.log(
+          "No such person found, creating dummy person for debugging"
+        );
+        this.setState({
+          mentor: {
+            bios: [{ title: "This is my Bio", bio: "I am a person" }],
+            firstName: "Dummy",
+            lastName: "Lname",
+            university: "GECH University",
+            email: "gpburdell@gatech.edu",
+            phone: "1234567"
+          }
+        });
+      });
+  }
 
   render() {
     return (
@@ -37,24 +53,12 @@ class Profile extends React.Component {
           <div className="profile-header">
             {/*<ImageDisplay id={1} className="img-responsive" width="200" height="200"/>*/}
 
-            <AboutHeader mentor={this.state.mentor}/>
-          </div>
-        </div>
-
-        <div className="row marketing">
-          <div className="col-lg-6">
-            
-            <BioContainer bioInfo={this.state.mentor.bios}/>
-
-          </div>
-
-          <div className="col-lg-6">
-
+            <ProfilePanel mentor={this.state.mentor} />
           </div>
         </div>
       </div>
-    )
+    );
   }
-};
+}
 
 export default Profile;
---
+++ frontend/containers/ProfilePanel.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { connect } from "react-redux";
+import { fetchMentor } from "../actions/mentorActions.js";
+import BioContainer from "./BioContainer";
+
+class ProfilePanel extends React.Component {
+  render() {
+    var info = this.props.mentor;
+    return (
+      <div id="user-profile">
+        <div className="top">
+          <h2>
+            {info.firstName} {info.lastName}
+          </h2>
+          <h3>{info.university}</h3>
+
+          <hr />
+          <p>
+            {info.email} | {info.phone}
+          </p>
+        </div>
+
+        <div className="bottom">
+          <h3>Bios</h3>
+          <BioContainer bioInfo={info.bios} />
+        </div>
+      </div>
+
+      // <div className="profile-about">
+      //     <label className="flex-item profile-name">{this.props.mentor.firstName} {this.props.mentor.lastName}</label>
+      //     <div className="display-flex">
+      //         <div className="flex-item">{this.props.mentor.university}</div>
+      //     </div>
+      //     <div className="flex-item">{this.props.mentor.email}</div>
+      //     <div className="flex-item">{this.props.mentor.phoneNumber}</div>
+      // </div>
+    );
+  }
+}
+
+export default ProfilePanel;
